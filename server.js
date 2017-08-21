2d378897a14acb08e1dde8d8549617fc0b37e0fb
Joel Kuzmarski <leoj3n@gmail.com>
2017-08-21 06:45:40 -0500

    Use new API

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
 const consoleApp = require('@fidojs/fidojs-kennel-console');
 const { GlitchPlease } = require('glitch-please');
 const path = require('path');
+const fs = require('fs');
 
 const please = new GlitchPlease({
-  appPath: path.join(__dirname, 'kennel'),
-  installedDirectory: ['node_modules', 'bit-docs', 'lib', 'configure', 'node_modules'],
-  installPatterns (pkg) {
-    return ['package.json'];
+  appRoot: path.join(__dirname, 'kennel'),
+  appIsInstalled (appRoot) {
+    return fs.existsSync(path.join(appRoot, 'node_modules', 'bit-docs', 'lib', 'configure', 'node_modules'));
   },
-  buildCommand: { cmd: 'npm', args: ['run', 'gen'] },
-  buildPatterns (pkg) {
-    // TODO: respect ignore pattern
-    return [pkg['bit-docs']['glob']['pattern'], 'patch'];
-  },
-  distIndex: 'index.html',
-  distDirectory (pkg) {
-    if (pkg['bit-docs'].hasOwnProperty('dest')) {
-      return pkg['bit-docs']['dest'];
+  distPath (appRoot, appPackageJSON) {
+    if (appPackageJSON['bit-docs'].hasOwnProperty('dest')) {
+      return path.join(appRoot, appPackageJSON['bit-docs']['dest']);
     } else {
-      return 'doc';
+      return path.join(appRoot, 'doc');
     }
   },
-  distRoute (pkg) {
-    if (pkg['bit-docs'].hasOwnProperty('dest')) {
-      return `/${pkg['bit-docs']['dest']}`;
+  distRoute (appPackageJSON) {
+    if (appPackageJSON['bit-docs'].hasOwnProperty('dest')) {
+      return '/' + appPackageJSON['bit-docs']['dest'];
     } else {
       return '/doc';
     }
   }
 });
 
+please.appWatchRun(function(appPackageJSON) {
+  return [appPackageJSON['bit-docs']['glob']['pattern']];
+}, { cmd: 'npm', args: ['run', 'fido'] });
+
 please.server.use('/', consoleApp);
-please.begin();
+please.startup();
